86f1ce23b66a03a2f4eea23103969504a4f2f039
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2023-02-13 14:05:57 +0530

    project skeleton

---
+++ init_setup.sh
@@ -0,0 +1,10 @@
+echo [$(date)]: "START"
+echo [$(date)]: "Creating env with python 3.8 version"
+conda create --name ppd python=3.8 -y
+echo [$(date)]: "Activating the environment"
+source activate ppd
+echo [$(date)]: "Installing the dev env"
+pip install -r requirements.txt
+echo [$(date)]: "Installing prediction env"
+pip install -r prediction_service/requirements_dev.txt
+echo [$(date)]: "END"
---
+++ template.py
@@ -0,0 +1,48 @@
+import logging
+import os
+from pathlib import Path
+
+package_name = "pdd" #phising_domain_detector
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/component/__init__.py",
+    f"src/{package_name}/utils/__init__.py",
+    f"src/{package_name}/config/__init__.py",
+    f"src/{package_name}/pipeline/__init__.py",
+    f"src/{package_name}/entity/__init__.py",
+    f"src/{package_name}/constants/__init__.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "configs/config.yaml",
+    "prediction_service/app.py",
+    "prediction_service/Dockerfile",
+    "prediction_service/requirements.txt",
+    "dvc.yaml",
+    "params.yaml",
+    "init_setup.sh",
+    "requirements.txt",
+    "setup.py",
+    "pyproject.toml",
+    "tox.ini",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath= Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating directory: {filedir}")
+    
+    else:
+        logging.info(f"{filename} already exists")
